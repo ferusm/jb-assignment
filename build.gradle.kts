99e98d3e1c3f500df38bb4cb823c31aee9734e0d
Iurii Nikolaev <nvyyvh@gmail.com>
2021-12-16 11:43:54 +0300

    Adding Gradle/Kotlin project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.github.ferusm.assignment.jetbrains"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+}
